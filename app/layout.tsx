466c9dabe241e83004cfa905ebafe541d4d99056
Kaiser styve <kaiserstyve2@gmail.com>
2025-06-04 15:03:51 +0100

    Update layout styling by adding overflow and full width/height classes to the body for improved layout management.

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="/assets/icon-loum-angel.png" type="image/x-icon" />
       </head>
-      <body className={poppins.className}>
+      <body className={poppins.className + ' overflow-hidden w-full h-full'}>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
